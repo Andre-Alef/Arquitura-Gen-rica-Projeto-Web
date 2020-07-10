f2dec534052becc3eb98e7e288c693c359942be9
Andre Alef Degaspari Moura <alef.moura@verithus.com.br>
2020-07-10 19:53:09 -0300

    first commit

---
+++ ArquiteturaTeste.Domain/Services/INovoChamado.cs
@@ -0,0 +1,20 @@
+﻿using ArquiteturaTeste.Domain.Models;
+using System.Collections.Generic;
+
+namespace ArquiteturaTeste.Domain.Services
+{
+    public interface INovoChamado
+    {
+        void CadastrarChamado(NovoChamado chamado);
+
+        void EditarStatus(NovoChamado chamado);
+
+        void CancelarChamado(NovoChamado chamado);
+
+        List<NovoChamado> ListarChamados();
+
+        NovoChamado ListarPorId(int id);
+
+
+    }
+}
---
+++ ArquiteturaTeste.Infrastructure/Repositories/RepositorioChamados.cs
@@ -0,0 +1,53 @@
+﻿using ArquiteturaTeste.Domain.Models;
+using ArquiteturaTeste.Domain.Services;
+using ArquiteturaTeste.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArquiteturaTeste.Infrastructure.Repositories
+{
+
+    public class RepositorioChamados : INovoChamado
+    {
+        private ContextChamadosTeste1 _context; 
+
+        public RepositorioChamados(ContextChamadosTeste1 context)
+        {
+            this._context = context;  
+        }
+
+        public void CadastrarChamado(NovoChamado chamado)
+        {
+            _context.chamados.Add(chamado);
+            _context.SaveChanges();
+        }
+
+        public void CancelarChamado(NovoChamado chamado)
+        {
+            _context.chamados.Remove(chamado);
+            _context.SaveChanges();
+        }
+
+        public void EditarStatus(NovoChamado chamado)
+        {
+            _context.Entry<NovoChamado>(chamado).State = System.Data.Entity.EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public List<NovoChamado> ListarChamados()
+        {
+            return _context.chamados.ToList();
+        }
+
+        public NovoChamado ListarPorId(int id)
+        {
+            return _context.chamados.Where(x => x.IdChamado == id).FirstOrDefault();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
